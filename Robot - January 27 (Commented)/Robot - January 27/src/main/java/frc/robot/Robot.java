36abc1bfdc93c820f43d19e613ca5c0f5b658aca
KyleDerbabian <kylederbabian@lhsla.org>
2020-02-15 18:15:42 -0800

    Update Robot.java

--- Robot - January 27 (Commented)/Robot - January 27/src/main/java/frc/robot/Robot.java
+++ Robot - January 27 (Commented)/Robot - January 27/src/main/java/frc/robot/Robot.java
@@ -1,38 +1,26 @@
-// Places the class Robot in the package frc.robot.
+// ???????????????????????????????????????????
 package frc.robot;
 
-// Imports the DriverStation class.
-import edu.wpi.first.wpilibj.DriverStation;
+// Imports the TimedRobot class.
+import edu.wpi.first.wpilibj.TimedRobot;
+
+// Imports the SendableChooser class.
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
 // Imports the SmartDashboard class.
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-// Imports the TimedRobot class.
-import edu.wpi.first.wpilibj.TimedRobot;
+// Imports the DriverStation class.
+import edu.wpi.first.wpilibj.DriverStation;
+
+// Imports the Joystick class.
+import edu.wpi.first.wpilibj.Joystick;
 
 // Imports the I2C class.
 import edu.wpi.first.wpilibj.I2C;
 
 // Imports the Color class.
 import edu.wpi.first.wpilibj.util.Color;
-
-// Imports the Joystick class.
-import edu.wpi.first.wpilibj.Joystick;
-
-// Imports the ControlMode class.
-import com.ctre.phoenix.motorcontrol.ControlMode;
-
-// Imports the TalonSRX class.
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
-// Imports the MotorType class.
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-// Imports the CANSparkMax class.
-import com.revrobotics.CANSparkMax;
-
-// Imports the CANEncoder class.
-import com.revrobotics.CANEncoder;
 
 // Imports the ColorSensorV3 class.
 import com.revrobotics.ColorSensorV3;
@@ -43,29 +31,80 @@ import com.revrobotics.ColorMatch;
 // Imports the ColorMatchResults class.
 import com.revrobotics.ColorMatchResult;
 
-// Creates the Robot class.
+// Imports the CANSparkMax class.
+import com.revrobotics.CANSparkMax;
+
+// Imports the MotorType class.
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+// Imports the CANEncoder class.
+import com.revrobotics.CANEncoder;
+
+// Imports the TalonSRX class.
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+// Imports the ControlMode class.
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+// Imports the PigeonIMU class.
+import com.ctre.phoenix.sensors.PigeonIMU;
+
+// ??????????????????????????????????????????????????????
 public class Robot extends TimedRobot {
 
-  // Creates a new Joystick object to represent the joystick controlling the robot.
-  Joystick joystick = new Joystick(0);
+  // Creates a new Joystick object to represent the first joystick controlling the robot.
+  Joystick joystick1 = new Joystick(0);
 
-  // Creates a new TalonSRX object to represent the Talon controlling the CONTROL PANEL mechanism.
-  TalonSRX controlPanelMechanism = new TalonSRX(5);
+  // Creates a new Joystick object to represent the second joystick controlling the robot.
+  Joystick joystick2 = new Joystick(1);
 
   // Creates a new CANSparkMax object to represent the SPARK MAX controlling the front-left drive train motor.
-  CANSparkMax FL = new CANSparkMax(8, MotorType.kBrushless);
+  CANSparkMax FL = new CANSparkMax(6, MotorType.kBrushless);
 
   // Creates a new CANSparkMax object to represent the SPARK MAX controlling the back-left drive train motor.
-  CANSparkMax BL = new CANSparkMax(9, MotorType.kBrushless);
+  CANSparkMax BL = new CANSparkMax(7, MotorType.kBrushless);
 
   // Creates a new CANSparkMax object to represent the SPARK MAX controlling the front-right drive train motor.
-  CANSparkMax FR = new CANSparkMax(10, MotorType.kBrushless);
+  CANSparkMax FR = new CANSparkMax(8, MotorType.kBrushless);
 
-  // Creates a new CANSparkMax object to represent the SPARK MAX controlling the back-right drive train motor.
-  CANSparkMax BR = new CANSparkMax(11, MotorType.kBrushless);
+  // Creates a new CANSparkMax object to represent the SPARK MAX controlling the back-left drive train motor.
+  CANSparkMax BR = new CANSparkMax(9, MotorType.kBrushless);
 
-  // Initializes a CANEncoder object to represent the encoder on the front-left drive train motor.
-  CANEncoder encoder = FL.getEncoder();
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // rename all motorcontrollers according to ARENA names.
+
+  // Creates a new CANSparkMax object to represent the SPARK MAX controlling the first shooter motor.
+  CANSparkMax shooter1 = new CANSparkMax(44, MotorType.kBrushless);
+
+  // Creates a new CANSparkMax object to represent the SPARK MAX controlling the second shooter motor.
+  CANSparkMax shooter2 = new CANSparkMax(55, MotorType.kBrushless);
+
+  // Creates a new CANEncoder object to represent the encoder on the shooter motors.
+  CANEncoder shooterEncoder = shooter1.getEncoder();
+  
+  // Creates a new TalonSRX object to represent the Talon controlling the first lift motor.
+  TalonSRX lift1 = new TalonSRX(1);
+
+  // Creates a new TalonSRX object to represent the Talon controlling the second lift motor.
+  TalonSRX lift2 = new TalonSRX(2);
+
+  // Creates a new TalonSRX object to represent the Talon controlling the CONTROL PANEL mechanism.
+  TalonSRX controlPanelMechanism = new TalonSRX(0);
+
+  // Creates a new PigeonIMU object to represent the gyroscope.
+  PigeonIMU gyroscope = new PigeonIMU(lift2);
+
+  // Declares a new double[] variable.
+  double[] ypr = new double[3];
+
+  // Declares a new double variable.
+  double power;
+
+  // Declares a new double variable. 
+  double leftTurnPower;
+
+  // Declares a new double variable.
+  double leftTurnConstant;
   
   // Initializes an I2C.Port object to represent the I2C port on the roboRIO.
   I2C.Port i2cPort = I2C.Port.kOnboard;
@@ -103,81 +142,78 @@ public class Robot extends TimedRobot {
   // Creates a new Color object to represent the color yellow.
   final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
-  //
   @Override
-
-  // Defines a method for actions involving the initialization of the robot.
   public void robotInit() {
 
     // Adds the color blue to the color matcher.
     colorMatcher.addColorMatch(kBlueTarget);
-    
+      
     // Adds the color red to the color matcher.
     colorMatcher.addColorMatch(kRedTarget);
 
     // Adds the color green to the color matcher.
     colorMatcher.addColorMatch(kGreenTarget);
-  
+
     // Adds the color yellow to the color matcher.
     colorMatcher.addColorMatch(kYellowTarget); 
-  
   }
 
-  //
   @Override
-  
-  // Defines a method for actions involving the initialization of autonomous mode.
+  public void robotPeriodic() {
+  }
+
+  @Override
   public void autonomousInit() {
-  
   }
 
-  //
   @Override
-
-  // Defines a method for actions involving autonomous mode.
   public void autonomousPeriodic() {
-  
   }
 
-  //
   @Override
-  
-  // Defines a method for actions involving the initialization of teleoperated mode.
   public void teleopInit() {
-  
+    FL.setInverted(false);
+    BL.setInverted(false);
+    FR.setInverted(true);
+    BR.setInverted(true);
   }
 
-  //
   @Override
-  
-  // Defines a method for actions involving teleoperated mode
   public void teleopPeriodic() {
 
-    if (joystick.getRawButton(3)) {
-      FL.restoreFactoryDefaults();
+    //SmartDashboard.putNumber("3:1 encoder", shooterEncoder.getVelocity());
+    //shooter1.set(-joystick1.getRawAxis(1));
+    //shooter2.set(joystick1.getRawAxis(1));
+
+    FL.set(joystick2.getRawAxis(1));
+    BL.set(joystick2.getRawAxis(1));
+    FR.set(joystick1.getRawAxis(1));
+    BR.set(joystick1.getRawAxis(1));
+
+    if (joystick1.getRawButton(1)) {
+      turnLeft(90);
     }
-    SmartDashboard.putNumber("Encoder Position", encoder.getPosition());
-    SmartDashboard.putNumber("Encoder Velocity", encoder.getVelocity());
 
-    FL.set(joystick.getRawAxis(1));
 
-    // Calls the method updateColors()
-    updateColors();
+    //rotationControl();
+    //positionControl();
 
-    // Calls the method rotationControl()
-    rotationControl();
-
-    // Calls the method positionControl()
-    positionControl();
+    //shooter1.set(-joystick1.getRawAxis(1));
+    //shooter2.set(joystick1.getRawAxis(1));
+    //if (joystick1.getRawButton(4)) {
+      //turnNinetyDegreesLeft();
+    //}
 
   }
 
-  //
+
+
+
+  /**
+   * This function is called periodically during test mode.
+   */
   @Override
-
-  // Defines a method for actions involving the test mode.
   public void testPeriodic() {
-
   }
 
   // Defines a method for updating color variables and displaying color variables on FRC Driver Station and FRC SmartDashboard.
@@ -290,7 +326,7 @@ public class Robot extends TimedRobot {
   public void rotationControl() {
 
     // Checks if 'B' is pressed in order to start the ROTATION CONTROL code.
-    if (joystick.getRawButton(2)) {
+    if (joystick1.getRawButton(2)) {
 
       // Loops through 16 iterations in order to spin the CONTROL PANEL approximately 4 times.
       for (int i = 0; i < 16; i++) {
@@ -299,7 +335,7 @@ public class Robot extends TimedRobot {
         updateColors();
 
         // Checks if 'A' is pressed in order to stop the CONTROL PANEL mechanism.
-        if (joystick.getRawButton(1)) {
+        if (joystick1.getRawButton(1)) {
 
           // Stops the CONTROL PANEL mechanism.
           controlPanelMechanism.set(ControlMode.PercentOutput, 0);
@@ -319,10 +355,10 @@ public class Robot extends TimedRobot {
             updateColors();
 
             // Spins the CONTROL PANEL mechanism. 
-            controlPanelMechanism.set(ControlMode.PercentOutput, 0.5);
+            controlPanelMechanism.set(ControlMode.PercentOutput, 0.4);
 
             // Checks if 'A' is pressed in order to stop the CONTROL PANEL mechanism.
-            if (joystick.getRawButton(1)) {
+            if (joystick1.getRawButton(1)) {
 
               // Stops the CONTROL PANEL mechanism.
               controlPanelMechanism.set(ControlMode.PercentOutput, 0);
@@ -346,10 +382,10 @@ public class Robot extends TimedRobot {
             updateColors();
 
             // Spins the CONTROL PANEL mechanism.
-            controlPanelMechanism.set(ControlMode.PercentOutput, 0.5);
+            controlPanelMechanism.set(ControlMode.PercentOutput, 0.4);
 
             // Checks is 'A' is pressed in order to stop the CONTROL PANEL mechanism.
-            if (joystick.getRawButton(1)) {
+            if (joystick1.getRawButton(1)) {
 
               // Stops the CONTROL PANEL mechanism.
               controlPanelMechanism.set(ControlMode.PercentOutput, 0);
@@ -363,20 +399,20 @@ public class Robot extends TimedRobot {
 
         } 
         
-        // Checks if the detected color is neither blue nor red in order to spin the CONTROL PANEL to either blue or red.
+        // Checks if the detected color is neither blue nor red in order to spin the CONTROL PANEL to either red.
         else {
 
-          // Loops as long as the detected color is neither red nor blue in order to spin the CONTROL PANEL to either red or blue.
-          while (match.color != kRedTarget || match.color != kBlueTarget) {
+          // Loops as long as the detected color is neither red nor blue in order to spin the CONTROL PANEL to red.
+          while (match.color != kRedTarget) {
 
             // Calls the method updateColors()
             updateColors();
 
             // Spins the CONTROL PANEL mechanism.
-            controlPanelMechanism.set(ControlMode.PercentOutput, 0.5);
+            controlPanelMechanism.set(ControlMode.PercentOutput, 0.4);
 
             // Checks if 'A' is pressed in order to stop the CONTROL PANEL mechanism.
-            if (joystick.getRawButton(1)) {
+            if (joystick1.getRawButton(1)) {
 
               // Stops the CONTROL PANEL mechanism.
               controlPanelMechanism.set(ControlMode.PercentOutput, 0);
@@ -392,6 +428,9 @@ public class Robot extends TimedRobot {
 
       }
 
+      // Stops the CONTROL PANEL mechanism.
+      controlPanelMechanism.set(ControlMode.PercentOutput, 0);
+
     }
 
   }
@@ -400,24 +439,122 @@ public class Robot extends TimedRobot {
   public void positionControl() {
     
     // Checks if 'Y' is pressed in order to start the POSITION CONTROL code.
-    if (joystick.getRawButton(4)) {
+    if (joystick1.getRawButton(4)) {
 
-      // Calls the method updateColors()
+      // Calls the method updateColors().
       updateColors();
 
-      // Spins the CONTROL PANEL mechanism.
-      controlPanelMechanism.set(ControlMode.PercentOutput, 0.5);
+      // Loops as long as the detected color is not the wanted color in order to align the specified color with the ARENA's color sensor.
+      while (!colorString.equals(wantedColorString)) {
 
-      // Checks to see if the wanted color is met in order to stop the CONTROL PANEL mechanism. 
-      if (colorString.equals(wantedColorString)) {
+        // Calls the method updateColors().
+        updateColors();
 
-        // Stops the CONTROL PANEL mechanism.
-        controlPanelMechanism.set(ControlMode.PercentOutput, 0);
+        // Spins the CONTROL PANEL mechanism.
+        controlPanelMechanism.set(ControlMode.PercentOutput, 0.3);
+
+        // Checks to see if button 1 on joystick1 is pressed in order to stop the CONTROL PANEL mechanism.
+        if (joystick1.getRawButton(1)) {
+
+          // Stops the CONTROL PANEL mechanism.
+          controlPanelMechanism.set(ControlMode.PercentOutput, 0);
+          
+          // Breaks the while loop.
+          break;
+
+        }
+
+      }
+
+      // Stops the CONTROL PANEL mechanism.
+      controlPanelMechanism.set(ControlMode.PercentOutput, 0);     
+
+    }
+
+  }
+
+  public void updateGyroscopeValues() {
+
+    // Updates the orientation array with the new values for yaw, pitch, and roll.
+    gyroscope.getYawPitchRoll(ypr);
+
+    // Prints the yaw value to the SmartDashboard.
+    SmartDashboard.putNumber("Yaw", ypr[0]);
+
+  }
+  
+  public void turnLeft(float numberOfDegrees) {
+    
+    // Sets the gyroscope's yaw value to 0.
+    gyroscope.setYaw(0, 10);
+
+    updateGyroscopeValues();
+
+    double yaw = ypr[0];
+
+    leftTurnConstant = 0.0006;
+
+    leftTurnPower = (numberOfDegrees - ypr[0]) * leftTurnConstant;
+
+    while (yaw >= -numberOfDegrees) {
+      
+      updateGyroscopeValues();
+
+      yaw = ypr[0];
+
+      FL.set(leftTurnPower);
+      BL.set(leftTurnPower);
+      FR.set(-leftTurnPower);
+      BR.set(-leftTurnPower);
+
+      if (joystick1.getRawButton(5)) {
+
+        FL.set(0);
+        BL.set(0);
+        FR.set(0);
+        BR.set(0);
+
+        break;
 
       }
 
     }
+    gyroscope.setYaw(0, 10);
 
+  }
+
+  // This method spins the robot ninety degrees. This method is old. I am writing a new one that's better.
+  public void turnNinetyDegreesLeft() {
+    gyroscope.setYaw(0, 10);
+    gyroscope.getYawPitchRoll(ypr);
+    double yaw;
+    power = 0.2;
+    while (ypr[0] >= -90) {
+      gyroscope.getYawPitchRoll(ypr);
+      yaw = ypr[0] + 0.001;
+      SmartDashboard.putNumber("Yaw", ypr[0]);
+      FL.set(power);
+      BL.set(power);
+      FR.set(-power);
+      BR.set(-power);
+      if (joystick1.getRawButton(2)) {
+        FL.set(0);
+        BL.set(0);
+        FR.set(0);
+        BR.set(0);
+        break;
+      }
+      power *= 0.99999;
+      SmartDashboard.putNumber("Power", power);
+    }
+    FL.set(-0.1);
+    BL.set(-0.1);
+    FR.set(0.1);
+    BR.set(0.1);
+    FL.set(0);
+    BL.set(0);
+    FR.set(0);
+    BR.set(0);
   }
 
 }
